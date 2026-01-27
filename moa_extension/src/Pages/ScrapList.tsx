9be555bde4db3e1085f87e8b5bdec9b7c3fe9e5b
beaksohyun12 <qorthgus1112@gmail.com>
2026-01-27 10:01:46 +0900

    style: 스크랩 카드 css 수정 및 드래그 정렬 추가

--- moa_extension/src/Pages/ScrapList.tsx
+++ moa_extension/src/Pages/ScrapList.tsx
@@ -2,6 +2,18 @@ import Empty from "../components/UI/Empty/Empty";
 import ScrapCard from "../components/UI/ScrapCard/ScrapCard";
 import type { Scrap } from "../types/scrap.domain";
 
+import {
+  DndContext,
+  closestCenter,
+} from "@dnd-kit/core";
+import type { DragEndEvent } from "@dnd-kit/core";
+
+import {
+  SortableContext,
+  verticalListSortingStrategy,
+  arrayMove,
+} from "@dnd-kit/sortable";
+
 interface ScrapListProps {
   scraps: Scrap[];
   setScraps: React.Dispatch<React.SetStateAction<Scrap[]>>;
@@ -9,6 +21,21 @@ interface ScrapListProps {
 }
 
 export default function ScrapList({ scraps, setScraps, highlightScrapId }: ScrapListProps) {
+
+  // 드래그 끝났을 때 순서 변경
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
+
+    if (!over || active.id === over.id) return;
+
+    setScraps((prev) => {
+      const oldIndex = prev.findIndex((s) => s.id === active.id);
+      const newIndex = prev.findIndex((s) => s.id === over.id);
+
+      return arrayMove(prev, oldIndex, newIndex);
+    });
+  };
+
   // 스크랩 삭제
   const handleDelete = (id: number) => {
     setScraps((prev) => prev.filter((scrap) => scrap.id !== id));
@@ -25,20 +52,30 @@ export default function ScrapList({ scraps, setScraps, highlightScrapId }: Scrap
         overflowY: "auto",
       }}
     >
-      {/* 스크랩 리스트 */}
       {scraps.length === 0 ? (
-          <Empty />
+        <Empty />
       ) : (
-        scraps.map((scrap, index) => (
-          <ScrapCard
-            key={scrap.id}
-            scrap={scrap}
-            index={index + 1}
-            onDelete={handleDelete}
-            highlighted={scrap.id === highlightScrapId}
-          />
-        ))
+        <DndContext
+          collisionDetection={closestCenter}
+          onDragEnd={handleDragEnd}
+        >
+          <SortableContext
+            items={scraps.map((scrap) => scrap.id)}
+            strategy={verticalListSortingStrategy}
+          >
+            {scraps.map((scrap, index) => (
+              <ScrapCard
+                key={scrap.id}
+                scrap={scrap}
+                index={index + 1}
+                onDelete={handleDelete}
+                highlighted={scrap.id === highlightScrapId}
+              />
+            ))}
+          </SortableContext>
+        </DndContext>
       )}
     </div>
   );
+
 }
