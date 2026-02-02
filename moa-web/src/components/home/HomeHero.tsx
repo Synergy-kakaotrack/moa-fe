7a35e8274592eccafdcb55a2449f85d900670750
beaksohyun12 <qorthgus1112@gmail.com>
2026-02-02 09:37:10 +0900

    fix: Moa user에서 Ryan으로 이름 수정

--- moa-web/src/components/home/HomeHero.tsx
+++ moa-web/src/components/home/HomeHero.tsx
@@ -5,11 +5,12 @@ type Props = {
   userName: string;
 };
 
+
 export default function HomeHero({ userName }: Props) {
   return (
     <section className={styles.hero}>
       <h1 className={styles.title}>
-        {userName}님, 오늘은 어떤 멋진 프로젝트를 만드실건가요?
+        Ryan님, 오늘은 어떤 멋진 프로젝트를 만드실건가요?
       </h1>
 
       <div className={styles.searchWrap}>
